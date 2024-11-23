f93348fa4e2e8114edf38152fb71b6ca293ed087
SeanKuehl <teddscottfelt@gmail.com>
2024-11-23 15:13:59 -0500

    got everything working with Postgres

--- index.js
+++ index.js
@@ -20,60 +20,23 @@ const limiter = RateLimit({
   max: 20,
 });
 
-// To connect with your mongoDB database
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI, {
-    dbName: 'ElmiraStories-DB',
-}) 
-  .then((res) => {
-    console.log("Database connected");
-    app.listen(5000, "::"); //this is to make it IPV6 for the Railway private network and reverse proxying
-    
+//connect to postgres database
+const pg = require('pg');
+//import pg from 'pg'
+const { Client } = pg
+const client = new Client({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB,
   })
-  .catch((error) => {
-    console.log(error);
-  });
+client.connect()
 
-//schema for memorials. This included memorial trees and benches
-const MemorialSchema = new mongoose.Schema({
-    memorial_ID: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    dedicated_to: {
-        type: String,
-        required: true,
-    },
-    dedicated_by: {
-        type: String,
-        required: true,
-        default: "Elmira Lions Club"
-    },
-    date_added: {
-        type: Date,
-        default: Date.now,
-    },
-    approximate_location: {
-        type: String,   //this will be longitude and latitude
-        required: true,
-    },
-    side_of_trail: {
-        type: String,   //this will be either left or right or N/A if it's a special situation
-        required: true,
-    },
-    additional_description: {
-        type: String,   //if there is something special of note or a story associated with the tree, otherwise N/A
-        
-    },
-    memorial_image: {
-        type: String,   
-        required: true,
-    }
+client.on('error', (err) => {
+    console.error('something bad has happened!', err.stack)
+  })
 
-});
-const Memorial = mongoose.model('memorial_trees', MemorialSchema);
-//Memorial.createIndexes();
 
 // For backend and express
 const express = require('express');
@@ -115,32 +78,7 @@ app.get("/", (req, resp) => {
     // backend working properly
 });
 
-app.post("/register_new_memorial", async (req, resp) => {
-    try {
-        
-        const new_memorial = new Memorial({
-            memorial_ID: req.body.memorialId,
-            dedicated_to: req.body.dedicatedTo,
-            dedicated_by: isEmpty(req.body.dedicatedBy) ? "Elmira Lions Club": req.body.dedicatedBy,
-            //date added has a good default, so no real need to pass it here
-            approximate_location: req.body.approximateLocation,
-            side_of_trail: isEmpty(req.body.sideOfTrail) ? "N/A": req.body.sideOfTrail,
-            additional_description: isEmpty(req.body.additionalDescription) ? "N/A": req.body.additionalDescription,
-            memorial_image: req.body.image,
-        });
-        let result = await new_memorial.save();
-        result = result.toObject();
-        if (result) {
-            resp.send(req.body);
-            console.log(result);
-        } else {
-            console.log("Memorial already registered");
-        }
 
-    } catch (e) {
-        resp.send("Something Went Wrong");
-    }
-});
 
 
 app.get('/get_memorial_image/:imageName', async (req, res) => {
@@ -167,37 +105,34 @@ app.get('/get_memorial_image/:imageName', async (req, res) => {
 
 
 app.get('/get_memorial_by_search_term/:searchTerm', async (req, res) => {
-    console.log("request recieved");
+    
+    
     //check if the search term is empty/null. If this is the case, return all
-    //if the search term isn't empty, search with it and return results
-
+    
     //this is the 'super secret' key that means the user didn't enter anything and we should return all entries
     //if someone manages to enter this in by pure chance, I will have already won the lottery six times in a row and be too rich to worry about it
     if (req.params.searchTerm !== "Dan Kuso The GOAT"){
-        //search with it
-        const results = await Memorial.find({
-            $or: [  //search the following fields non-exclusively
-                {dedicated_to: { "$regex": req.params.searchTerm, "$options": "i" }},  //this will search the field for entries that contain the search term
-                {dedicated_by: { "$regex": req.params.searchTerm, "$options": "i" }},
-                {additional_description: { "$regex": req.params.searchTerm, "$options": "i" }}
-            ]
-        }).sort()
         
-        res.send(results);
+        const data = ["%"+req.params.searchTerm+"%"];   //the percentages are the postgres similarity operator, so they can search for "sarah" and get "sarah fuller" as a result for instance
+        const result = await client.query("SELECT * FROM MemorialTrees WHERE LOWER(dedicated_to) LIKE  LOWER($1) OR LOWER(dedicated_to) LIKE LOWER($1) OR LOWER(additional_description) LIKE LOWER($1)", data);
+        
+        
+        res.send(result.rows);
     }
     else {
         //search all
-        const results = await Memorial.find({})
-
-        res.send(results);
+        const result = await client.query('SELECT * FROM MemorialTrees');
+        res.send(result.rows);
     }
         
+
+    
 
 
 });
 
 
 
-
+app.listen(5000, "::");
 
 
